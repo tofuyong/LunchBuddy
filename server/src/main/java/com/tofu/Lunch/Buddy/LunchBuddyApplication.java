e90190b71d19d6db3ef489cc541547ce39bd1fb3
your_name <tofuyong@gmail.com>
2023-07-11 07:49:06 +0800

    first commit

---
+++ server/src/main/java/com/tofu/Lunch/Buddy/LunchBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.tofu.Lunch.Buddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchBuddyApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/tofu/Lunch/Buddy/LunchBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tofu.Lunch.Buddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
